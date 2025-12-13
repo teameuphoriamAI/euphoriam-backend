5156eb2604f00668710b452a6ad5c1ac18b99469
hareemYashal <reachyashal1@gmail.com>
2025-12-13 23:22:02 +0500

    added dignostic functions

---
+++ src/routes/diagnosticRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const asyncHandler = require("../helpers/asyncHandler");
+const auth = require("../middleware/auth");
+const requireRole = require("../middleware/requireRole");
+const diagnosticController = require("../controllers/diagnosticController");
+
+const router = express.Router();
+
+router.get(
+  "/admin",
+  auth,
+  requireRole(["admin"]),
+  asyncHandler(diagnosticController.listAll)
+);
+router.get("/", auth, asyncHandler(diagnosticController.listMine));
+router.get("/:id", auth, asyncHandler(diagnosticController.getById));
+router.post(
+  "/run-diagnostic",
+  asyncHandler(diagnosticController.createDiagnostic)
+);
+
+module.exports = router;
